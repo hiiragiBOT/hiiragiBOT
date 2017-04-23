320f62d854c540411dff3074214619ad5a892ed7
柊BOT 柊 <kirito032771@icloud.com>
2017-04-23 03:00:57 -0500

    追記01

--- app.js
+++ app.js
@@ -14,6 +14,38 @@ var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
+// 追記箇所
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+// 追記箇所2
+var request = require('request');
+
+app.post('/api', function(req, res) {
+  var options = {
+    method: 'POST',
+    uri: 'https://api.line.me/v2/bot/message/reply',
+    body: {
+      replyToken: req.body.events[0].replyToken,
+      messages: [{
+        type: "text",
+        text: req.body.events[0].message.text
+      }]
+    },
+    auth: {
+      bearer: 'Channel Access Token' // ここは自分のtokenに書き換える
+    },
+    json: true
+  };
+  request(options, function(err, res, body) {
+    console.log(JSON.stringify(res));
+  });
+  res.send('OK');
+});
+// --------ここまで追加--------
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
